1d8acb64f55f19a92ef03aa00f64b826b38d73c4
peakcary <peakcary@163.com>
2018-05-07 14:28:59 +0800

    update lucky draw

--- js/luckydraw/index.js
+++ js/luckydraw/index.js
@@ -9,7 +9,7 @@ $(function () {
 		colors: [],					//大转盘奖品区块对应背景颜色
 		outsideRadius: 192,			//大转盘外圆的半径
 		textRadius: 155,				//大转盘奖品位置距离圆心的距离
-		insideRadius: 78,			//大转盘内圆的半径
+		insideRadius: 68,			//大转盘内圆的半径
 		startAngle: 0,				//开始角度
 		bRotate: false				//false:停止;ture:旋转
 	};
@@ -29,7 +29,7 @@ $(function () {
 		$('#wheelcanvas').stopRotate();
 		$('#wheelcanvas').rotate({
 			angle: 0,
-			animateTo: angles + 1800,
+			animateTo: angles + 2520,
 			duration: 8000,
 			callback: function () {
 				alert(txt);
@@ -45,7 +45,7 @@ $(function () {
 		// var item = rnd(1, turnplate.restaraunts.length);
 		// //奖品数量等于10,指针落在对应奖品区域的中心角度[252, 216, 180, 144, 108, 72, 36, 360, 324, 288]
 		// rotateFn(item, turnplate.restaraunts[item - 1]);
-var item=10;
+		var item = 10;
 		switch (item) {
 			case 1:
 				rotateFn(1, turnplate.restaraunts[0]);
@@ -97,7 +97,7 @@ function drawTurnplate(turnplate) {
 	$('#wheelcanvas').attr('height', turnplate.wh + 'px');
 
 	//动态添加大转盘的奖品与奖品区域背景颜色
-	turnplate.restaraunts = ["50M免费流量包", "10闪币", "谢谢参与", "5闪币", "10M免费流量包", "20M免费流量包", "20闪币 ", "30M免费流量包", "100M免费流量包", "2闪币"];
+	turnplate.restaraunts = ["50M免费流量包免费流量", "10闪币", "谢谢参与", "5闪币", "10M免费流量包", "20M免费流量包", "20闪币 ", "30M免费流量包", "100M免费流量包", "2闪币"];
 	turnplate.colors = ["#FFF4D6", "#FFFFFF", "#FFF4D6", "#FFFFFF", "#FFF4D6", "#FFFFFF", "#FFF4D6", "#FFFFFF", "#FFF4D6", "#FFFFFF"];
 
 	var canvas = document.getElementById('wheelcanvas');
@@ -127,30 +127,51 @@ function drawTurnplate(turnplate) {
 			//----绘制奖品开始----
 			ctx.fillStyle = "#E5302F";
 			var text = turnplate.restaraunts[i];
-			var line_height = 17;
+			var line_height = 18;
 			//translate方法重新映射画布上的 (0,0) 位置
 			ctx.translate(turnplate.wh / 2 + Math.cos(angle + arc / 2) * turnplate.textRadius, turnplate.wh / 2 + Math.sin(angle + arc / 2) * turnplate.textRadius);
 
 			//rotate方法旋转当前的绘图
 			ctx.rotate(angle + arc / 2 + Math.PI / 2);
 
+
 			/** 下面代码根据奖品类型、奖品名称长度渲染不同效果，如字体、颜色、图片效果。(具体根据实际情况改变) **/
 			if (text.indexOf("M") > 0) {//流量包
-				var texts = text.split("M");
-				for (var j = 0; j < texts.length; j++) {
-					ctx.font = j == 0 ? 'bold 20px Microsoft YaHei' : '16px Microsoft YaHei';
-					if (j == 0) {
-						ctx.fillText(texts[j] + "M", -ctx.measureText(texts[j] + "M").width / 2, j * line_height);
-					} else {
-						ctx.fillText(texts[j], -ctx.measureText(texts[j]).width / 2, j * line_height);
-					}
-				}
-			} else if (text.indexOf("M") == -1 && text.length > 6) {//奖品名称长度超过一定范围 
-				text = text.substring(0, 6) + "||" + text.substring(6);
-				var texts = text.split("||");
-				for (var j = 0; j < texts.length; j++) {
-					ctx.fillText(texts[j], -ctx.measureText(texts[j]).width / 2, j * line_height);
-				}
+				var text1="谢谢谢谢嘻谢";
+				ctx.fillText(text1, -ctx.measureText(text1).width / 2,  0);
+
+				var text2="参与再次谢";
+				ctx.fillText(text2, -ctx.measureText(text2).width / 2,  line_height);
+				
+				var text3="参与再次";
+				ctx.fillText(text3, -ctx.measureText(text3).width / 2,  2*line_height);
+
+
+				var text4="参再次";
+				ctx.fillText(text4, -ctx.measureText(text4).width / 2,  3*line_height);
+
+				// var texts = text.split("M");
+
+				// for (var j = 0; j < texts.length; j++) {
+				// 	ctx.font = j == 0 ? 'bold 20px Microsoft YaHei' : '16px Microsoft YaHei';
+				// 	if (j == 0) {
+				// 		ctx.fillText(texts[j] + "M", -ctx.measureText(texts[j] + "M").width / 2, j * line_height);
+				// 	} else {
+				// 		ctx.fillText(texts[j], -ctx.measureText(texts[j]).width / 2, j * line_height);
+				// 	}
+				// }
+			} else if (text.indexOf("M") == -1 && text.length > 10) {//奖品名称长度超过一定范围 
+				// text = text.substring(0, 3) + "||" + text.substring(3, 10) + "||" + text.substring(10);
+				// var texts = text.split("||");
+				// for (var j = 0; j < texts.length; j++) {
+				// 	ctx.fillText(texts[j], -ctx.measureText(texts[j]).width / 2, j * line_height);
+				// }
+			} else if (text.indexOf("M") == -1 && text.length > 6 && text.length < 10) {//奖品名称长度超过一定范围 
+				// text = text.substring(0, 6) + "||" + text.substring(6);
+				// var texts = text.split("||");
+				// for (var j = 0; j < texts.length; j++) {
+				// 	ctx.fillText(texts[j], -ctx.measureText(texts[j]).width / 2, j * line_height);
+				// }
 			} else {
 				//在画布上绘制填色的文本。文本的默认颜色是黑色
 				//measureText()方法返回包含一个对象，该对象包含以像素计的指定字体宽度
